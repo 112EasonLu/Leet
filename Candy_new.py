fee2d7bca6d7850ca32859324d65e3f9d6220f1f
盧易聖 <luyisheng@luyishengde-MacBook-Pro.local>
2019-07-24 03:31:44 +0800

    update

--- Candy_new.py
+++ Candy_new.py
@@ -1,27 +1,33 @@
-class Solution(object):
-    def candy(self, ratings):
-        """
-        :type ratings: List[int]
-        :rtype: int
-        """
-        need=[1]*len(ratings)
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        bratings=[ratings[i] for i in range(-1,-len(ratings)-1,-1)]
+        need=[]
         forward=[]
         backward=[]
         for i in range(len(ratings)):
+            print('i:',i)
             for j in range(i+1,len(ratings)):
-                count=0
-                if ratings[i]>ratings[j] and ratings[j]!=ratings[j+1]:
-                    count+=1
+                print('j:',j)
+                if j!=len(ratings)-1:
+                    count,count1=0,0
+                    if ratings[i]>ratings[j] and ratings[j]!=ratings[j+1]:
+                        count+=1
+                    else:break
+                    if bratings[i]>bratings[j] and bratings[j]!=bratings[j+1]:
+                        count1+=1
+                    else:break
                 else:
-                    break
-                forward.append(count)
-                count=0
-            for k in range(-i-1,-len(ratings),-1):
-                if ratings[-i-1]>ratings[k] and ratings[k]!=ratings[k-1]:
-                    count+=1
-                else:
-                    break
-                backward.append(count)
-                count=0
-        new=[(1+max[forward[i],backrward[i]]) for i in range(len(ratings))]
-        print(new)
+                    count,count1=0,0
+                    if ratings[i]>ratings[j]:
+                        count+=1
+                    else:break
+                    if bratings[i]>bratings[j]:
+                        count1+=1
+                    else:break
+            forward.append(count)
+            print('forward:',forward)
+            backward.append(count1)
+            print('backward:',backward)
+        need=[(1+max(forward[i],backward[i])) for i in range(len(backward))]
+        print(need)
+        return sum(need)
